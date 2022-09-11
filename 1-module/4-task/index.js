22fede9433fd5be4b51516475b9afe1a0bf4f460
art-kopylov1997 <art.kopylov1997@mail.ru>
2022-09-11 22:50:01 +0300

    реализовал функцию поиска спама в строке

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,8 @@
 function checkSpam(str) {
   // ваш код...
+  let newStr = str.toLowerCase();
+  let firstSearchStr = "1xBet".toLowerCase();
+  let secondSearchStr = "XXX".toLowerCase();
+
+  return newStr.indexOf(firstSearchStr) >= 0 || newStr.indexOf(secondSearchStr) >= 0;
 }
