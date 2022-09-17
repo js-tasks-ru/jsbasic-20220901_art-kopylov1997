e032ac241408694da85aeb8b3a30274970987cf4
art-kopylov1997 <art.kopylov1997@mail.ru>
2022-09-17 22:05:15 +0300

    написал функцию для суммирования зарплат

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,13 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (salaries[key] === Number(salaries[key])
+      && salaries[key] !== Infinity
+      && salaries[key] !== -Infinity
+      && salaries[key] !== isNaN(salaries[key])) {
+      sum += salaries[key];
+    }
+  }
+
+  return sum;
 }
