61cf6b208cf403f9af14e404bcc2c15776119f9f
art-kopylov1997 <art.kopylov1997@mail.ru>
2022-09-25 21:18:59 +0300

    создал функцию возвращающую имя и баланс пользователей

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,12 @@
 function showSalary(users, age) {
   // ваш код...
+  let result = '';
+
+  users.forEach(el => {
+    if (el.age <= age) {
+      result += `${el.name}, ${el.balance}\n`;
+    }
+  });
+
+  return result.substring(0, result.length - 1);
 }
