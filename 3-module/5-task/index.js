6927df3f8b1e58f284f43a6ee1a609d5744a0060
art-kopylov1997 <art.kopylov1997@mail.ru>
2022-09-25 21:34:25 +0300

    написал функцию для нахождения min, max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,17 @@
 function getMinMax(str) {
   // ваш код...
+  let arr = str.split(' ');
+  let arrNumbers = [];
+
+  arr.forEach(el => {
+    if (Number(el)) {
+      arrNumbers.push(Number(el));
+    }
+  });
+
+  const obj = {};
+  obj.min = Math.min(...arrNumbers);
+  obj.max = Math.max(...arrNumbers);
+
+  return obj;
 }
