386c48528cf6c20cc5d089f163d9533a90321205
art-kopylov1997 <art.kopylov1997@mail.ru>
2022-09-25 20:52:11 +0300

    написал функцию для для изменения строки

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,16 +1,9 @@
 function camelize(str) {
   // ваш код...
-  let arr = [];
-  arr.push(str.split('-'));
-
-  for (let i in arr) {
-    let newArr = arr[i].slice(1);
-
-    for (let k in newArr) {
-      let res = newArr[k].slice(0, 1).toUpperCase() + newArr[k].slice(1);
-      return res;
-
-    }
-  }
-
+  return str
+    .split('-')
+    .map(
+      (word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join('')
 }
